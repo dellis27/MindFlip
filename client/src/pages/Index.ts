81bfaf70d487249a2cd92acea9370cec48c2df0d
dellis27 <cooperdellis2772@gmail.com>
2025-05-22 18:33:12 -0500

    work from tuesday

---
+++ client/src/pages/Index.ts
@@ -0,0 +1,7 @@
+export { default as FlashCards } from './FlashCards';
+export { default as Category } from './Category';
+export { default as HomePage } from './HomePage'
+export { default as Landing } from './Landing'
+export { default as List } from './List'
+export { default as NotFound } from './NotFound'
+export { default as Quiz } from './Quiz'
