c435db0cca4897d64a70f3f7f7bf5a1eac0aae0e
Cooper Laptop <cooperdellis2772@gmail.com>
2025-05-27 15:13:19 -0600

    fixing nav issue and working on header nav css

--- client/src/components/Header.tsx
+++ client/src/components/Header.tsx
@@ -12,6 +12,22 @@ function Header() {
             <h1>MindFlip</h1>
             <h3> An all in one study guide</h3>
             </div>
+
+            <nav className='header-nav'> {/* This will be the nav containing the information commented out on App.tsx, for testing purposes. This Nav information should be living inside of the header or the footer component.*/}
+                <ul>
+                    <li><Link to='/about'>ABOUT-US</Link></li>
+                    <li><Link to='/contact'>CONTACT</Link></li>
+                    <li></li>
+                </ul>
+            </nav>
+
+            <div className='button-box'>
+            <button className='login-button'> {/*Login button, nested in the header, this might need to be its own component to correctly handle the cases of users being logged in/out*/}
+            <img className='login-icon' src='../assets/login.svg'/>
+            <h3>Login</h3>
+            <h3>Sign-Up</h3>
+            </button>
+            </div>
             
         </header>
     )
